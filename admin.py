dfd72bebe7cd75b7dbf2898660163d38854af19c
Tesmic <58257845+Tesmic@users.noreply.github.com>
2019-12-02 11:51:35 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,11 @@
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import Note
+
+class NoteAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Note
+
+admin.site.register(Note,NoteAdmin)
